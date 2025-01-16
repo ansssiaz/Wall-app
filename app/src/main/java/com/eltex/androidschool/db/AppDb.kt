76cddcddab677072bd1dbc4450c429883d8f6eb8
Змеевская Анастасия <zmeevskaya1@yandex.ru>
2025-01-16 23:18:10 +0700

    Дз 21 Room - Prepopulate, Migrations

--- app/src/main/java/com/eltex/androidschool/db/AppDb.kt
+++ app/src/main/java/com/eltex/androidschool/db/AppDb.kt
@@ -1,18 +1,26 @@
 package com.eltex.androidschool.db
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteColumn
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
 import com.eltex.androidschool.dao.EventDao
 import com.eltex.androidschool.entity.Converters
 import com.eltex.androidschool.entity.EventEntity
 
 @Database(
     entities = [EventEntity::class],
-    version = 1,
+    version = 3,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = AppDb.MyExampleAutoMigration::class)
+    ]
 )
+
 @TypeConverters(Converters::class)
 abstract class AppDb : RoomDatabase() {
     abstract val eventDao: EventDao
@@ -31,8 +39,8 @@ abstract class AppDb : RoomDatabase() {
 
                 val appDb =
                     Room.databaseBuilder(application, AppDb::class.java, "eltex_android_app_db")
-                        .fallbackToDestructiveMigration()
                         .allowMainThreadQueries()
+                        .createFromAsset("data_for_prepopulating.db")
                         .build()
 
                 INSTANCE = appDb
@@ -40,5 +48,10 @@ abstract class AppDb : RoomDatabase() {
                 return appDb
             }
         }
+    }
+
+    @DeleteColumn("Events", "views")
+    class MyExampleAutoMigration : AutoMigrationSpec {
+
     }
 }
